49fdfbc8f40cc342db1e1286bb2749f3ae42e684
gregj77 <greg_j@live.com>
2014-12-23 22:21:20 +0100

    Added depencency to RX.

--- App01/Program.cs
+++ App01/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reactive.Linq;
 
 namespace App01
 {
@@ -10,6 +11,9 @@ namespace App01
     {
         static void Main(string[] args)
         {
+            
+            //System.Reactive.Linq;
+            //Observable.
         }
     }
 }
